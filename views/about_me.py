b78d046a21488fddc43b3399477a6ef2ebc3c075
Lanre Adetola <r0913836@student.thomasmore.be>
2025-02-19 13:54:35 +0100

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -2,27 +2,26 @@ import streamlit as st
 
 from forms.contact import contact_form
 
-@st.dialog("Contact Me")
 def show_contact_form():
     contact_form()
 
 # -- Me --
-col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+col1, col2 = st.columns(2)
 with col1:
     st.image("assets/profilepic.png", width=230)
 with col2:
-    st.title("Lanre Adetola", anchor=False)
+    st.title("Lanre Adetola")
     st.write(
         "Data Analyst/Data Scientist Intern, seeking to assist enterprises by supporting data-driven decision-making."
     )
-    if st.button(" Contact Me"):
+    if st.button("Contact Me"):
         show_contact_form()
 
 
 #--- Experience---
 
 st.write("\n")
-st.subheader("Experience & Qualifications", anchor=False)
+st.subheader("Experience & Qualifications")
 st.write(
     """
     - Experienced in deriving actionable insights from complex data sets
@@ -37,11 +36,11 @@ st.write(
 #---Skills---
 
 st.write("\n")
-st.subheader("Hard Skills", anchor=False)
+st.subheader("Hard Skills")
 st.write(
     """
     - **Programming Languages**: Python (with libraries such as **Scikit-learn**, **Pandas**), **SQL**, **C#**
-    - **Data Visualization**: Expertise in **Power BI**, **MS Excel**,**Matplot** ,**Plotly** for creating interactive and insightful visualizations
+    - **Data Visualization**: Expertise in **Power BI**, **MS Excel**, **Matplot**, **Plotly** for creating interactive and insightful visualizations
     - **Machine Learning & Modeling**: Experience with **Logistic Regression**, **Linear Regression**, **Decision Trees** for predictive modeling
     - **Databases**: Proficient in **MySQL**, **LiteDB**, **MongoDB** for data storage and management
     """
